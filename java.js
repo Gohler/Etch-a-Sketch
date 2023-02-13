b152e5bb455f30405ab4d9e6fccfdd93bb1637ba
Gohler <gochpatryk@gmail.com>
2023-02-13 18:35:54 +0100

    Create necessary functions for sketch to run. Mainly random color generator and new grid function

--- java.js
+++ java.js
@@ -1,14 +1,28 @@
-for (c=0;c<256;c++) {
-const container = document.querySelector("#container");
-const cell = document.createElement("square");
-container.appendChild(cell);
-}
+startingGrid();
 
-let grid = document.querySelectorAll("square");
-grid.forEach(square => {
-    square.addEventListener('mouseover',() => {
-        square.style.backgroundColor = generateRandomColorRgb();
+let newGrid = document.querySelector('#erase');
+newGrid.addEventListener('click',() =>{
+    container.replaceChildren();
+    let answer = prompt("How many squares you want to have per side?");
+    if (answer<0 || answer>50 || isNaN(answer)) {
+        alert("Must be a number between 1 and 100");
+        startingGrid();
+    } else {
+        for (c=0;c<answer*answer;c++) {
+            const container = document.querySelector("#container");
+            container.style.gridTemplateColumns = `repeat(${answer}, 1fr)`
+            container.style.gridTemplateRows = `(${answer}, 1fr)`
+            const cell = document.createElement("square");
+            container.appendChild(cell);
+
+            let grid = document.querySelectorAll("square");
+            grid.forEach(square => {
+            square.addEventListener('mouseover',() => {
+            square.style.backgroundColor = generateRandomColorRgb();
     });
+});
+        }
+    }
 });
 
 function generateRandomColorRgb() {
@@ -16,6 +30,18 @@ function generateRandomColorRgb() {
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
     return "rgb(" + red + ", " + green + ", " + blue + ")";
-  }
+    }
 
-
+function startingGrid(){
+    for (c=0;c<256;c++) {
+    const container = document.querySelector("#container");
+    const cell = document.createElement("square");
+    container.appendChild(cell);
+    }
+    let grid = document.querySelectorAll("square");
+    grid.forEach(square => {
+        square.addEventListener('mouseover',() => {
+        square.style.backgroundColor = generateRandomColorRgb();
+        });
+    });
+ }
